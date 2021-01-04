e7bdf2cb6425ae4d859f1a6aaaa81a5b299920d1
Chryssa Aliferi <c.aliferi@gmail.com>
2021-01-04 16:42:11 +0200

    Creates EmojiValues.kt

---
+++ emojibottomsheetdialog/src/main/java/com/withplum/emojibottomsheetdialog/emoji/EmojiValues.kt
@@ -0,0 +1,5 @@
+package com.withplum.emojibottomsheetdialog.emoji
+
+const val PROVIDE_AUTHORITY_FONT = "com.google.android.gms.fonts"
+const val PROVIDE_PACKAGE_FONT = "com.google.android.gms"
+const val QUERY_FONT = "Noto Color Emoji Compat"
