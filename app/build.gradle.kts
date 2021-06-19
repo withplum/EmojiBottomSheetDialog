735bbc1e26c4bff5ba77095613132883732fe76b
Chris Margonis <chris.margonis@gmail.com>
2021-06-19 16:07:16 +0300

    Migrate example build files to kotlin dsl

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,25 +4,24 @@ plugins {
 }
 
 android {
-
-    compileSdkVersion 29
+    compileSdkVersion(29)
 
     defaultConfig {
-        applicationId "com.withplum.emojiapp"
-        minSdkVersion 23
-        targetSdkVersion 29
+        applicationId = "com.withplum.emojiapp"
+        minSdkVersion(23)
+        targetSdkVersion(29)
     }
 
     buildTypes {
-        release {
-            minifyEnabled false
+        getByName("release") {
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
 
 dependencies {
-    implementation project(":emojiBottomSheetDialog")
+    implementation(project(":emojiBottomSheetDialog"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.21")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.appcompat:appcompat:1.2.0")
