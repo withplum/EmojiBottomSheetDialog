16b850905fd96e7aa420a1f6d036349b0fa2cde2
Chris Margonis <chris.margonis@withplum.com>
2024-08-01 15:59:21 +0300

    Update refreshVersions plugin to 0.60.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.40.2"
+    id("de.fayard.refreshVersions") version "0.60.5"
 }
 
 include(":emojiBottomSheetDialog", ":app")
