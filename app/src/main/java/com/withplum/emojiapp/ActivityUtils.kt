9d5f3e085c8a08d70ad9643476adaf9a32323869
Chris Margonis <chris.margonis@withplum.com>
2024-08-01 16:30:30 +0300

    Remove unnecessary type casting

--- app/src/main/java/com/withplum/emojiapp/ActivityUtils.kt
+++ app/src/main/java/com/withplum/emojiapp/ActivityUtils.kt
@@ -6,6 +6,5 @@ import androidx.annotation.IdRes
 import com.withplum.emojibottomsheetdialog.view.recyclerview.unsafeLazy
 
 fun <T : View> Activity.bind(@IdRes idRes: Int): Lazy<T> {
-    @Suppress("UNCHECKED_CAST")
-    return unsafeLazy { findViewById<T>(idRes) }
+    return unsafeLazy { findViewById(idRes) }
 }
