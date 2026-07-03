06ff396f0adbba5352f3b2fba8ce0393b79008b2
Chris Margonis <chris.margonis@withplum.com>
2026-07-03 09:50:38 +0300

    Migrate away from legacy plugin management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
-buildscript {
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
-    }
-}
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.gradle.maven.publish) apply false
     alias(libs.plugins.dokka) apply false
 }
